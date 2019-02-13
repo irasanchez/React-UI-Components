bcdd7a9d5ec73a08ead6fba80c5547c49f0dc41f
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-13 12:15:17 -0700

    CardContent set up

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardContent = () => {
+  return (
+    <section>
+      <h3>Get started with React</h3>
+      <p>
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application
+      </p>
+      <address>reactjs.org</address>
+    </section>
+  );
+};
+
+export default CardContent;
