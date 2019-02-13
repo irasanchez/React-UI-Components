308890f52359bbf5530ce8f087008e1513fb7521
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-13 12:00:20 -0700

    ImageThumbnail set up

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Header.css";
 
+import ImageThumbnail from "./ImageThumbnail";
+
 const HeaderContainer = () => {
   return (
     <header className="header-container">
