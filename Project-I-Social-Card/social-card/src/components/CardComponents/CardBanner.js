cf441d6c7957f176f4de6ab03ac5d88de8ced4fa
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-13 12:11:50 -0700

    set up CardBanner

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,8 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardBanner = () => {
+  <div className="card-banner" />;
+};
+
+export default CardBanner;
