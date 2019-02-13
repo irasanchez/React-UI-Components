f6d3179be670be27dafd05b588d1deec4881a059
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-13 11:58:17 -0700

    HeaderContainer set up

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderContainer = () => {
+  return (
+    <header className="header-container">
+      <ImageThumbnail />
+      <HeaderTitle />
+      <HeaderContent />
+    </header>
+  );
+};
+
+export default HeaderContainer;
