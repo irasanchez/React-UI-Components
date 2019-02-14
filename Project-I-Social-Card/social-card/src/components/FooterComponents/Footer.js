f50b3c69893610ccd94444c36ed61993f2127e4b
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-13 18:28:00 -0700

    Footer icons displayed; Footer styled; Footer rendered in App.js

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,4 +1,15 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Footer.css";
 
-const Footer = () => {};
+const Footer = () => {
+  return (
+    <footer>
+      <i className="far fa-comment icon" />
+      <i className="fas fa-sync-alt icon" />
+      <i className="far fa-heart icon" />
+      <i className="far fa-envelope icon" />
+    </footer>
+  );
+};
+
+export default Footer;
